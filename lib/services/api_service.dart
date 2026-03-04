7f7adaaa3748e3910560d0961682a24ee36e17be
Nixacon25 <contact@nixacom.com>
2026-03-04 15:45:19 +0000

    feat: update Android API base URL to Render deployment

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -8,9 +8,9 @@ class ApiService {
   static String get baseUrl {
     if (kIsWeb)
       return 'http://192.168.1.69:8082/api/v1';
-      // return 'https://bawane-api.up.railway.app/api/v1';
     else if (Platform.isAndroid) {
-      return 'http://192.168.1.69:8082/api/v1';
+      // return 'http://192.168.1.69:8082/api/v1';
+      return 'https://bawane.onrender.com/api/v1';
     }
     return 'http://localhost:8082/api/v1';
   }
